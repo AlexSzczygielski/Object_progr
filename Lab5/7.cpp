f5c3dcb02285bf33d310887e2515c1353c56b168
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-02 22:04:40 +0100

    7.cpp a)

--- Lab5/7.cpp
+++ Lab5/7.cpp
@@ -6,8 +6,19 @@ class NewString : public string{
     //a
     NewString(const char* str = "") : string(str){}
     NewString(const string& str = "") : string (str){}
+
+    bool contains(const char* input){
+        return this->find(input) != string::npos;
+    }
 };
 
 int main(){
+    NewString str("example_here_string");
+    //a
+    if(str.contains("here")){
+        cout << "here found in str!" << endl;
+    }else{
+        cout << "here not found in str!" << endl;
+    }
     return 0;
 }
