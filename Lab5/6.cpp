f711121b5dbffc00a119e8499a8f3b61c25161fd
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-02 21:23:22 +0100

    6.cpp

---
+++ Lab5/6.cpp
@@ -0,0 +1,5 @@
+/*
+Class circle should inherit from Figure, as it inherits coordinates and label. Rectangle and Square have
+properties that circle doesn't have (height, width, etc.), therefore it should'nt and can't inherit from 
+the above. The proper approach would be to create new class Circle, which would inherit from Figure.
+*/
