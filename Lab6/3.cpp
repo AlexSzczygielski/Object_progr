dd84a59129490fc6cb1730b13266d37cdcba26c6
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-10 21:43:25 +0100

    4. and a), upgraded invalid classes

--- Lab6/3.cpp
+++ Lab6/3.cpp
@@ -3,7 +3,7 @@
 class InvalidIndex{
     const char* m_error_msg;
     public:
-    InvalidIndex(const char* error) : m_error_msg(error){}
+    InvalidIndex(const char* error = "Invalid index") : m_error_msg(error){}
     const char* what(){
         std::cout << "invalid index exception: ";
         return m_error_msg;
@@ -13,14 +13,29 @@ class InvalidIndex{
 class InvalidValue{
     const char* m_error_msg;
     public:
-    InvalidValue(const char* error) : m_error_msg(error){}
+    InvalidValue(const char* error = "Invalid value") : m_error_msg(error){}
     const char* what(){
         std::cout << "invalid value exception: ";
         return m_error_msg;
     }
 };
 
-
+//4.
+class StaticArray{
+    unsigned int m_arr[16];
+    public:
+    StaticArray(){
+        for(int i = 0; i<16; i++){
+            std::cout << i << std::endl;
+            m_arr[i] = i;
+        }
+    }
+    int at(int index){
+        if(index<0 || index>15) throw InvalidIndex();
+        return m_arr[index];
+    }
+    
+};
 
 int main(){
     return 0;
