fd73340ddea5be044dcb8accf8d1b4ab5789062b
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-10 19:20:31 +0100

    2.cpp e) - fixed

--- Lab6/2.cpp
+++ Lab6/2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<exception>
 using namespace std;
 
 int main(){
@@ -14,9 +15,14 @@ int main(){
     stringstream s;
     s << std::hex << n;
     //d
-    cout << "n(hex): " << s.str();
-    //e (uncomment)
-    //str = "2147483648(MaxInt + 1)Bakczysaraj";
-    //n = std::stoi(str); //exception out of range
+    cout << "n(hex): " << s.str() << endl;
+    //e 
+    try{
+        str = "2147483648(MaxInt + 1)Bakczysaraj";
+        n = std::stoi(str); //exception out of range
+    }
+    catch(std::out_of_range& exception){
+        cerr << "You ran out of integer range: " << exception.what() << endl;
+    }
     return 0;
 }
