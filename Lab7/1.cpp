ad9f52c49278db34392e1a470b15008ba74534eb
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-20 15:28:04 +0100

    1) and 2)

---
+++ Lab7/1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+
+int main(){
+    //1
+    std::vector<std::string> shopping_list;
+    //2
+    shopping_list.push_back("wine");
+    shopping_list.push_back("beetroot");
+    return 0;
+}
