8e8d10fcf3bf777f437a0d1e774346970e3f0482
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-20 15:46:11 +0100

    c,d,e

--- Lab7/1.cpp
+++ Lab7/1.cpp
@@ -2,10 +2,32 @@
 #include<vector>
 
 int main(){
-    //1
+    //a
     std::vector<std::string> shopping_list;
-    //2
+    //b
     shopping_list.push_back("wine");
     shopping_list.push_back("beetroot");
+    shopping_list.push_back("bananas");
+    shopping_list.push_back("sugar");
+    shopping_list.push_back("chocolate");
+    shopping_list.push_back("carrot");
+
+    //c
+    std::cout<<"last element on the list: " << shopping_list.back() << std::endl;
+    shopping_list.pop_back();
+    std::cout<<"last element after deletion: " << shopping_list.back() << std::endl;
+
+    //d
+    shopping_list.insert(shopping_list.begin() + 2,"coffee");
+    std::cout << "3rd element: " << shopping_list.at(2) << std::endl;
+
+    //e
+    for (int i = 0; i<shopping_list.size();i++){
+        if(shopping_list.at(i) == "sugar"){
+            shopping_list.at(i) = "candy";
+            std::cout << "sugar replaced with candy" << std::endl;
+        }
+        //std::cout << shopping_list.at(i) << std::endl;
+    }
     return 0;
 }
