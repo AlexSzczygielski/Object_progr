373907f8b7cc101c92c746889d4f3d62ca5a7228
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-24 00:31:25 +0100

    3.cpp final

--- Lab7/3.cpp
+++ Lab7/3.cpp
@@ -9,8 +9,21 @@ int main(){
         int tel_number;
         std::cout << "Enter the name: ";
         std::cin >> name;
+        
+        //exit condition
+        if(name == "exit"){
+            std::cout << "terminating" << std::endl;
+            break; 
+        }
+
+        //a
         if(phonebook.count(name)){ //check if name exists in the map (true if yes, 0 if not)
-            std::cout << "here";
+            std::cout << phonebook[name] << std::endl;
+        } else{
+            //b
+            std::cout << "record doesn't exist, provide the telephone number now:" << std::endl;
+            std::cin >> tel_number;
+            phonebook[name] = tel_number; //create new record
         }
     }
     return 0;
