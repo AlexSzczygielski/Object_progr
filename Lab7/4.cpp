ba02f3022bb868b9138078918020d468b02740c1
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-24 02:02:11 +0100

    improved performance

--- Lab7/4.cpp
+++ Lab7/4.cpp
@@ -3,25 +3,17 @@
 #include<algorithm>
 #include<map>
 
-void print_map(std::map<char,int>& m){
+void print_map(const std::map<char,int>& m){
     for(auto& pair : m){
         std::cout << "frequency of '" << pair.first << "' : " << pair.second << std::endl;
     }
 }
 
-void letter_frequency(std::string& s){
-    char temp = s[0];
-    int count;
+void letter_frequency(const std::string& s){
     std::map<char,int> frequency_map; //map - keys strings and values integers
     
     for(int i = 0; i<s.size();i++){
-        if(frequency_map.count(s[i])==1){
-            //record exists
-            frequency_map[s[i]] = frequency_map.at(s[i])+1;
-        }else{
-            //record doesn't exist
-            frequency_map[s[i]] = 1;
-        }
+        frequency_map[s[i]]++;
     }
     print_map(frequency_map);
 }
