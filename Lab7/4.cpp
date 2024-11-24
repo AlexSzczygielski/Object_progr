fda3fdb0a795abbf1a9b74f57c8db1bc0f535e5e
AlexSzczygielski <theolekpl.szkolny@gmail.com>
2024-11-24 01:57:51 +0100

    frequency of letters using map, attempt 1

--- Lab7/4.cpp
+++ Lab7/4.cpp
@@ -3,19 +3,27 @@
 #include<algorithm>
 #include<map>
 
+void print_map(std::map<char,int>& m){
+    for(auto& pair : m){
+        std::cout << "frequency of '" << pair.first << "' : " << pair.second << std::endl;
+    }
+}
+
 void letter_frequency(std::string& s){
     char temp = s[0];
     int count;
     std::map<char,int> frequency_map; //map - keys strings and values integers
     
-    for(int i = 0; i<=s.size();i++){
-        if(s[i]==temp){
-            count++;
+    for(int i = 0; i<s.size();i++){
+        if(frequency_map.count(s[i])==1){
+            //record exists
+            frequency_map[s[i]] = frequency_map.at(s[i])+1;
         }else{
-            frequency_map[temp] = count;
-            std::cout << 
+            //record doesn't exist
+            frequency_map[s[i]] = 1;
         }
     }
+    print_map(frequency_map);
 }
 
 int main(){
